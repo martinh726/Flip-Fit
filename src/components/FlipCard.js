e086854fa1f1998db17d5acf27b812c11178197a
Martin Hernandez <mahernandez1665@gmail.com>
2025-04-09 23:17:02 -0700

    Copied original files to react components

---
+++ src/components/FlipCard.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./FlipCard.css";
+
+function FlipCard() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div className="flip-card" onClick={handleFlip}>
+      <div className={`flip-card-inner ${isFlipped ? "flipped" : ""}`}>
+        <div className="front">
+          <section id="text">Click to Reveal Exercise</section>
+        </div>
+        <div className="back">
+          <section id="workout">
+            <p>
+              Push-ups <br />4 X 20
+            </p>
+            <figure>
+              <img src="/imgs/pushup-1462808858.gif" alt="pushups-gif" />
+            </figure>
+            <input type="button" value="Add Timer" id="timer-button" />
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FlipCard;
